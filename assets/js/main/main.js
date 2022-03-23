5fa90a3492bc6e4be094d37e16bc1288042228ad
RenatoLima0 <renatoslima46@gmail.com>
2022-03-23 17:13:05 -0300

    home desktop

---
+++ assets/js/main/main.js
@@ -0,0 +1,48 @@
+if(document.querySelector('.nav-princ')){
+  const ativaMenu = () => {
+    const menuHamburger = document.querySelector('.menu-hamburguer');
+    const itemsMenu = document.querySelector('.items-menu');
+    
+    const clickMenu = () => {
+      menuHamburger.classList.toggle('ativo');
+      itemsMenu.classList.toggle('ativo');
+    }
+  
+    ['touchstart', 'click'].forEach(userEvent => {
+      menuHamburger.addEventListener(userEvent, clickMenu);
+    });
+  }
+  
+  ativaMenu();  
+}
+
+$('.owl-carousel-main').owlCarousel({
+  loop: true,
+  margin: 24,
+  nav: false,
+  responsive: {
+    0:{
+      items:1
+    },
+    600:{
+      items:3
+    },
+    1000:{
+      items:4.2
+    }
+  }
+})
+
+$('.owl-carousel-relat').owlCarousel({
+  loop: true,
+  margin: 48,
+  nav: false,
+  responsive: {
+    0:{
+      items:1
+    },
+    600:{
+      items:3
+    }
+  }
+})
