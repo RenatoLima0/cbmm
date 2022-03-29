38650fc72a7c02269cc0dcd32ffd2238854c304f
RenatoLima0 <renatoslima46@gmail.com>
2022-03-28 22:36:28 -0300

    ajustes caixa flutuante, navegação e mobile

--- assets/js/main/main.js
+++ assets/js/main/main.js
@@ -18,18 +18,47 @@ if(document.querySelector('.caixa-flutuante')){
 
   const animaCol = () => {
     const mainContent = document.querySelector('.main-content');
+    const navegacaoPags = document.querySelector('.navegacao-pags')
     const caixaFlut = document.querySelector('.caixa-flutuante');
     const sectionTop = mainContent.getBoundingClientRect().top;
+    // console.log(sectionTop);
     
-    if(sectionTop < 70){
+    if(sectionTop < 100){
       caixaFlut.classList.add('fixa');
-    } else if (sectionTop > 70) {
+      navegacaoPags.classList.add('fixa');
+    } else if (sectionTop > 100) {
       caixaFlut.classList.remove('fixa');
+      navegacaoPags.classList.remove('fixa');
     } 
   }
 
+  // const ativaItemFlutuante = () => {
+  //   const listaCaixa = document.querySelectorAll('.caixa-flutuante li');
+  //   const titlesMain = document.querySelectorAll('.main-content h3.main-title');
+
+  //   const limpaLista = () => {
+  //     listaCaixa.forEach(item => item.classList.remove('ativo'));
+  //   }
+
+  //   titlesMain.forEach((title, index) => {
+  //     const titleTop = title.getBoundingClientRect().top;
+  //     console.log('item ' + index + ' ' + titleTop);
+
+  //     if(titleTop < 300){
+  //       limpaLista();
+  //       listaCaixa[index].classList.add('ativo');
+  //     } else if (titleTop < 0){
+  //       limpaLista();
+  //     }
+  //   })
+
+  // }
+  
   window.addEventListener('scroll', animaCol);
+  // window.addEventListener('scroll', ativaItemFlutuante);
+  
 }
+
 
 $('.owl-carousel-main').owlCarousel({
   loop: false,
